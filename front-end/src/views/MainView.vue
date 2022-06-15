35fef2612098c5e8d5650a74ccc4703ca7171b94
iooe9094 <99378766+iooe9094@users.noreply.github.com>
2022-06-15 17:29:43 +0900

    220615_SpringBoot_Pagination

---
+++ front-end/src/views/MainView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <!-- Navigation-->
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="#page-top"
+          ><img src="@/assets/Test2/img/navbar-logo.svg" alt="..."
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse bg-warning" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#services">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <HeaderCom></HeaderCom>
+    <SectionCom></SectionCom>
+    <FooterCom></FooterCom>
+    <PortfolioCom></PortfolioCom>
+  </div>
+</template>
+
+<script>
+import loadWin from "@/assets/Test2/js/scripts";
+import HeaderCom from "@/components/main/HeaderCom.vue";
+import SectionCom from "@/components/main/SectionCom.vue";
+import FooterCom from "@/components/main/FooterCom.vue";
+import PortfolioCom from "@/components/main/PortfolioCom.vue";
+
+export default {
+  name: "Test2View",
+  components: { HeaderCom, SectionCom, FooterCom, PortfolioCom },
+  mounted() {
+    loadWin;
+  },
+};
+</script>
+
+<style scoped>
+@import "@/assets/Test2/css/styles.css";
+</style>
